bad583264e8c143b37196497e518dabd416a4991
viren-vii <virubhosale112@gmail.com>
2021-04-14 17:10:58 +0530

    Bluetooth added and 3 category separation

--- codetoar/index.js
+++ codetoar/index.js
@@ -1,7 +1,20 @@
+var mybutton;
+var lineFollowerCheck, ultrasoniceCheck, bluetoothCheck;
+
 window.onload = function() {
     addIfBlockRef = document.getElementById("ifBlocks");
     document.getElementById("cover").style.display = "none";
     document.getElementById("loader").style.display = "none";
+
+
+    mybutton = document.getElementById("myBtn");
+    //Get the button:
+
+
+
+    lineFollowerCheck = document.getElementById('line-follower-check');
+    ultrasoniceCheck = document.getElementById('ultrasonic-check');
+    bluetoothCheck = document.getElementById('bluetooth-check');
 
     handleOnChange();
     changeArduino();
@@ -30,6 +43,7 @@ var addedIf = {}, addedIfString = "";
             <option value=\"right\">right</option>
             <option value=\"stop\">stop</option>
 </select>
+value=\"0\"
 */
 function createIfBlock(idCount){
     //code if
@@ -40,7 +54,7 @@ function createIfBlock(idCount){
 
     return [
     "\
-    if(S1 == <input class=\"SS\" id=\"S1if"+idCount+"\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S2 == <input class=\"SS\" id=\"S2if"+idCount+"\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S3 == <input class=\"SS\" id=\"S3if"+idCount+"\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S4 == <input class=\"SS\" id=\"S4if"+idCount+"\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S5 == <input class=\"SS\" id=\"S5if"+idCount+"\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/>){<br>\
+    if(S1 == <input class=\"SS\" id=\"S1if"+idCount+"\" value=\"0\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S2 == <input class=\"SS\" id=\"S2if"+idCount+"\" value=\"0\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S3 == <input class=\"SS\" id=\"S3if"+idCount+"\" value=\"0\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S4 == <input class=\"SS\" id=\"S4if"+idCount+"\" value=\"0\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/> && S5 == <input class=\"SS\" id=\"S5if"+idCount+"\" value=\"0\" type=\"number\" min=\"0\" max=\"1\" oninput=\"handleOnChange();this.style.width = ((this.value.length +2) * 20) + 'px';\" required/>){<br>\
         <select name=\"functions\" id=\"Fnif"+idCount+"\" oninput=\"checkIfValues();\">\
             <option value=\"forward\" selected>forward</option>\
             <option value=\"reverse\">reverse</option>\
@@ -96,7 +110,7 @@ function checkIfValues(){
 }
 
 
-var visibleUS = false;
+var visibleUS = false, visibleIR = false;
 var usBlock = "";
 var vDist, vFnUS;
 
@@ -110,70 +124,47 @@ function toggleUS(){
 }
 
 var vTRIGGER_PIN, vECHO_PIN, vDist;
-var vIR1, vIR2, vIR3, vIR4, vIR5, vENA, vENB, vRM1, vRM2, vLM1, vLM2, vSP_ENA, vSP_ENB;
+var vIR1, vIR2, vIR3, vIR4, vIR5, vENA, vENB, vRM1, vRM2, vLM1, vLM2, vSP_EN1, vSP_EN2;
 var vfRM1, vfRM2, vfLM1, vfLM2,
     vrRM1, vrRM2, vrLM1, vrLM2, vrENA, vrENB,
     vlRM1, vlRM2, vlLM1, vlLM2, vlENA, vlENB,
     vd360RM1, vd360RM2, vd360LM1, vd360LM2, vd360ENA, vd360ENB;
-var vS1if0, vS2if0, vS3if0, vS4if0, vS5if0, vFnif0;
-function handleOnChange(){
+var vS1if0=0, vS2if0=0, vS3if0=0, vS4if0=0, vS5if0=0, vFnif0;
 
-    vDist = document.getElementById("dist").value;
-    var eus = document.getElementById("FnUS");
-    // console.log(e);
-    vFnUS = eus.options[eus.selectedIndex].text;
-    
-    
-    vSP_EN1 = document.getElementById("SP_EN1").value;
-    vSP_EN2 = document.getElementById("SP_EN2").value;
+var usDistFunction = `float c = 0;
+    int dist(){
+    digitalWrite(TRIGGER_PIN,LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIGGER_PIN,HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER_PIN,LOW);
+    int dur=pulseIn(ECHO_PIN, HIGH);
+    c=dur*0.0294/2;
+    return c;
+}`;
 
-    vS1if0 = document.getElementById("S1if0").value;
-    vS2if0 = document.getElementById("S2if0").value;
-    vS3if0 = document.getElementById("S3if0").value;
-    vS4if0 = document.getElementById("S4if0").value;
-    vS5if0 = document.getElementById("S5if0").value;
+var usPins = `    
+#define TRIGGER_PIN 9
+#define ECHO_PIN 10 `;
 
-    var eif = document.getElementById("Fnif0");
-    vFnif0 = eif.options[eif.selectedIndex].text;
-
-    checkIfValues();
-
-    usBlock = `if(dist()<`+vDist+`){
-            `+vFnUS+`();
-            delay(10);
-        }`;
-    changeArduino();
-}
-function stringify(){
-    addedIfString = "";
-    for(x in addedIf){
-        addedIfString += addedIf[x];
-    }
-    changeArduino();
-}
-//`+vv+`
-function changeArduino(){
-    code = `#define TRIGGER_PIN 9
-#define ECHO_PIN 10 
-
-#define IR1 A1
+var irPins = `#define IR1 A1
 #define IR2 A2
 #define IR3 A3
 #define IR4 A4
-#define IR5 A5
+#define IR5 A5`;
 
-#define EN2 3
+var motorPins=`#define EN2 3
 #define RM1 4
 #define RM2 5
 #define EN1 6 
 #define LM1 7
-#define LM2 8
+#define LM2 8`;
 
-float SP_EN1 = `+vSP_EN1+`;
-float SP_EN2 = `+vSP_EN2+`;
-float c = 0;
+function allFunctions(){
+    piece =  `float SP_EN1 = `+vSP_EN1+`;
+float SP_EN2 = `+vSP_EN2+`;`;
 
-
+return piece +`
 void forward(){
     Serial.print("Forwards Triggered");
     digitalWrite(RM1,HIGH);
@@ -232,54 +223,96 @@ void stop(){
     digitalWrite(LM2,LOW);
     analogWrite(EN1,0);
     analogWrite(EN2,0);
+}`;
 }
+function handleOnChange(){
 
 
+    vDist = document.getElementById("dist").value;
+    var eus = document.getElementById("FnUS");
+    // console.log(e);
+    vFnUS = eus.options[eus.selectedIndex].text;
+    
+    
+    vSP_EN1 = document.getElementById("SP_EN1").value;
+    vSP_EN2 = document.getElementById("SP_EN2").value;
+
+    vS1if0 = document.getElementById("S1if0").value;
+    vS2if0 = document.getElementById("S2if0").value;
+    vS3if0 = document.getElementById("S3if0").value;
+    vS4if0 = document.getElementById("S4if0").value;
+    vS5if0 = document.getElementById("S5if0").value;
+
+    var eif = document.getElementById("Fnif0");
+    vFnif0 = eif.options[eif.selectedIndex].text;
+
+    checkIfValues();
+
+    usBlock = `if(dist()<`+vDist+`){
+            `+vFnUS+`();
+            delay(10);
+        }`;
+    changeArduino();
+}
+function stringify(){
+    addedIfString = "";
+    for(x in addedIf){
+        addedIfString += addedIf[x];
+    }
+    changeArduino();
+}
+//`+vv+`
+function changeArduino(){
+
+code = ``+(visibleUS ? usPins : "")+`
+`+(visibleIR ? irPins : "")+`
+`+(visibleUS || visibleIR ? motorPins : "")+`
+`+(visibleUS || visibleIR ? allFunctions() : "")+`
 void setup(){
     Serial.begin(9600);
+    `+(!visibleUS && !visibleIR? `pinMode(LED_BUILTIN, OUTPUT);` : "")+`
+    `+(visibleUS || visibleIR ? `
     // MOTOR AND ENABLES
     pinMode (RM1, OUTPUT);
     pinMode (RM2, OUTPUT);
     pinMode (LM1, OUTPUT);
     pinMode (LM2, OUTPUT);
     pinMode (EN1, OUTPUT);
-    pinMode (EN2, OUTPUT);
+    pinMode (EN2, OUTPUT);` : "")+`
+    `+(visibleIR ? `
     // IR ARRAY
     pinMode (IR1, INPUT);
     pinMode (IR2, INPUT);
     pinMode (IR3, INPUT);
     pinMode (IR4, INPUT);
-    pinMode (IR5, INPUT);
+    pinMode (IR5, INPUT);` : "")+`
+    `+(visibleUS ? `
     // ULTRASONIC SENSOR
     pinMode(ECHO_PIN,INPUT);
-    pinMode(TRIGGER_PIN,OUTPUT);
+    pinMode(TRIGGER_PIN,OUTPUT);` : "")+`
 }
-
-int dist(){
-    digitalWrite(TRIGGER_PIN,LOW);
-    delayMicroseconds(2);
-    digitalWrite(TRIGGER_PIN,HIGH);
-    delayMicroseconds(10);
-    digitalWrite(TRIGGER_PIN,LOW);
-    int dur=pulseIn(ECHO_PIN, HIGH);
-    c=dur*0.0294/2;
-    return c;
-}
+`+(visibleUS ? usDistFunction : "")+`
 void loop(){
-	int S1 = digitalRead(IR1);
+    `+(!visibleUS && !visibleIR? `
+    digitalWrite(LED_BUILDIN,HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILDIN,LOW);
+    delay(1000);` : "")+`
+
+    `+(visibleUS ? usBlock : "")+`
+    `+(visibleIR ? `
+    int S1 = digitalRead(IR1);
 	int S2 = digitalRead(IR2);
 	int S3 = digitalRead(IR3);
 	int S4 = digitalRead(IR4);
-        int S5 = digitalRead(IR5);
-    
-        `+(visibleUS ? usBlock : "")+`
+    int S5 = digitalRead(IR5);
 
 	if(S1 == `+vS1if0+` and S2 == `+vS2if0+` and S3 == `+vS3if0+` and S4 == `+vS4if0+` and S5 == `+vS5if0+`){
             `+vFnif0+`();
 	}
     `+addedIfString+`
-}
-    `;
+    ` : "")+`
+}`;
     document.getElementById("arduinoCodeDiv").innerText = code;
 
 }
@@ -301,3 +334,170 @@ function copyToCB(containerid){
       }
 }
 
+
+
+function choiceCheck(){
+    // alert("functions");
+    if(lineFollowerCheck.checked){
+        visibleIR = true;
+    }else{
+        visibleIR = false;
+    }
+    if(ultrasoniceCheck.checked){
+        visibleUS = true;
+    }else{
+        visibleUS = false;
+    }
+    if(bluetoothCheck.checked){
+        visibleUS = false;
+        visibleIR = false;
+        handleBtChanges();
+        return;
+    }
+    handleOnChange();
+}
+
+var uVal,dVal,rVal,lVal,sVal,srVal,slVal;
+
+function handleBtChanges(){
+    u = document.getElementById('btControlsU');
+    d = document.getElementById('btControlsD');
+    r = document.getElementById('btControlsR');
+    l = document.getElementById('btControlsL');
+    s = document.getElementById('btControlsS');
+    sr = document.getElementById('btControlsSR');
+    sl = document.getElementById('btControlsSL');
+
+    uVal = u.options[u.selectedIndex].text;
+    dVal = d.options[d.selectedIndex].text;
+    rVal = r.options[r.selectedIndex].text;
+    lVal = l.options[l.selectedIndex].text;
+    sVal = s.options[s.selectedIndex].text;
+    srVal = sr.options[sr.selectedIndex].text;
+    slVal = sl.options[sl.selectedIndex].text;
+
+    btCodeHandler();
+}
+
+function btCodeHandler(){
+
+    code = `#define EN2 3 // right
+    #define RM1 4
+    #define RM2 5
+    #define EN1 6 // left
+    #define LM1 7
+    #define LM2 8
+    
+    float SP_EN1 = 20;
+    float SP_EN2 = 20;
+    
+    void forward(){
+        Serial.print("Forwards Triggered");
+        digitalWrite(RM1,HIGH);
+        digitalWrite(RM2,LOW);
+        analogWrite(EN2,SP_EN2);
+        digitalWrite(LM1,HIGH);
+        digitalWrite(LM2,LOW);
+        analogWrite(EN1,SP_EN1);   
+    }
+    
+    void right(){
+        Serial.print("Right Triggered");
+        digitalWrite(RM1,LOW);
+        digitalWrite(RM2,LOW);
+        analogWrite(EN2,0);
+        digitalWrite(LM1,HIGH);
+        digitalWrite(LM2,LOW);
+        analogWrite(EN1,SP_EN1);
+    }
+    
+    void left(){
+        Serial.print("Right Triggered");
+        digitalWrite(RM1,HIGH);
+        digitalWrite(RM2,LOW);
+        analogWrite(EN2,SP_EN2);
+        digitalWrite(LM1,LOW);
+        digitalWrite(LM2,LOW);
+        analogWrite(EN1,0);
+    }
+    
+    void deg360(){
+        Serial.print("Right Triggered");
+        digitalWrite(RM1,HIGH);
+        digitalWrite(RM2,LOW);
+        analogWrite(EN2,SP_EN2);
+        digitalWrite(LM1,LOW);
+        digitalWrite(LM2,HIGH);
+        analogWrite(EN1,SP_EN1);
+    }
+    
+    void reverse(){
+        Serial.print("Reverse Triggered");
+        digitalWrite(RM1,LOW);
+        digitalWrite(RM2,HIGH);
+        analogWrite(EN2,SP_EN2);
+        digitalWrite(LM1,LOW);
+        digitalWrite(LM2,HIGH);
+        analogWrite(EN1,SP_EN1);   
+    }
+    
+    void stop(){
+        Serial.print("Right Triggered");
+        digitalWrite(RM1,LOW);
+        digitalWrite(RM2,LOW);
+        digitalWrite(LM1,LOW);
+        digitalWrite(LM2,LOW);
+        analogWrite(EN1,0);
+        analogWrite(EN2,0);
+    }
+    
+    char recieved = 's';
+    
+    void setup(){
+        Serial.begin(9600);
+        // MOTOR AND ENABLES
+        pinMode (RM1, OUTPUT);
+        pinMode (RM2, OUTPUT);
+        pinMode (LM1, OUTPUT);
+        pinMode (LM2, OUTPUT);
+        pinMode (EN1, OUTPUT);
+        pinMode (EN2, OUTPUT);
+    }
+    void loop(){
+    
+        if(Serial.available() > 0){
+            recieved = Serial.read();
+            Serial.print(recieved);
+            Serial.print("\n");
+            if(recieved == 'u')`+uVal+`();  
+            else if(recieved == 'd')`+dVal+`();
+            else if(recieved == 'r')`+rVal+`();
+            else if(recieved == 'l')`+lVal+`();
+            else if(recieved == 's')`+sVal+`();
+            else if(recieved == 'sr')`+srVal+`();
+            else if(recieved == 'sl')`+slVal+`();
+        }
+    
+    }`;
+    document.getElementById("arduinoCodeDiv").innerText = code;
+}
+
+
+
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } 
