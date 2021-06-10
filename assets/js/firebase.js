8a97262d810f5776a95d55ff3bed3864ca699a04
viren-vii <56278281+viren-vii@users.noreply.github.com>
2021-06-10 15:12:15 +0530

    recaptcha testing

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -32,13 +32,13 @@ function storeToFirebase(rootDoc, rootCollection){
         email = document.getElementById('newsletterEmail').value;
     }else if(rootDoc === 'review'){
         message = document.getElementById('review').value;
-        name = document.getElementById('name').value;
+        fullName = document.getElementById('name').value;
         city = document.getElementById('city').value;
-        email = name = document.getElementById('email').value;
+        email = document.getElementById('email').value;
     }else if(rootDoc === 'iWantBot'){
         email = document.getElementById('emailBuy').value;
     }else if(rootDoc === 'getCallBack'){
-        name = document.getElementById('inputEmail4').value;
+        fullName = document.getElementById('inputEmail4').value;
         parentsName = document.getElementById('inputPassword4').value;
         parentsContact = document.getElementById('inputAddress').value;
         email = document.getElementById('inputAddress2').value;
@@ -56,6 +56,7 @@ function storeToFirebase(rootDoc, rootCollection){
     db.collection("emailFunctions").doc(rootDoc).collection(rootCollection).doc(email).set({data})
     .then(() => {
         console.log("Document successfully written!");
+        alert("Submitted Successfully! THank you!")
     })
     .catch((error) => {
         console.error("Error writing document: ", error);
